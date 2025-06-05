0c69d366ee61fca814226767b8dc946fe3e42e1d
tim <tim@flashlearn.ch>
2025-06-05 09:04:53 +0200

    .env

--- backend/src/main/java/ch/mensaapp/api/MensaApiApplication.java
+++ backend/src/main/java/ch/mensaapp/api/MensaApiApplication.java
@@ -1,5 +1,6 @@
 package ch.mensaapp.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +8,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class MensaApiApplication {
+    static {
+        Dotenv dotenv = Dotenv
+                .configure()
+                .load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MensaApiApplication.class, args);
     }
